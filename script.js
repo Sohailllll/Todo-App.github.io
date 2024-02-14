1f1e2634c2d0612e2d6f6b7ba54f640cd99da14c
Sohailllll <alamsohail734@gmail.com>
2024-02-14 16:47:52 +0530

    todo-app
    commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const inputBox = document.querySelector(".input-box");
+const listContainer = document.querySelector(".list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Write Something");
+  } 
+  
+  else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+
+
+    // for cross to delete the item
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span)
+  }
+
+  inputBox.value = "";
+};
+
+listContainer.addEventListener("click", (e)=> {
+    if(e.target.tagName == "LI"){
+        e.target.classList.toggle("checked")
+    }
+    else if(e.target.tagName == "SPAN"){
+        e.target.parentElement.remove()
+    }
+})
